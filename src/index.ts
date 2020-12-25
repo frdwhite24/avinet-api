546d2572629b5a9d4f03816a923d30400c8156f5
Fred White <fred-white@hotmail.co.uk>
2020-12-25 15:31:50 +0000

    :white_check_mark: exporting main for supertest prior to listening on port

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ import buildSchema from "./schema/index";
 import { UserModel } from "./schema/users/model";
 import { MyToken } from "./types";
 
-const main = async () => {
+export const main = async (): Promise<void> => {
   await connect();
 
   const app = express();
@@ -34,9 +34,11 @@ const main = async () => {
 
   apolloServer.applyMiddleware({ app });
 
-  app.listen(PORT, () =>
-    console.log(`Server ready at http://localhost:${PORT}/graphql`)
-  );
+  if (process.env.NODE_ENV !== "test") {
+    app.listen(PORT, () =>
+      console.log(`Server ready at http://localhost:${PORT}/graphql`)
+    );
+  }
 };
 
 main().catch((err) => {
