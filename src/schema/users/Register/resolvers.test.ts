9c48bd8ddf7a21ebafa9905d3216b625806712c6
Fred White <fred-white@hotmail.co.uk>
2020-12-28 21:02:22 +0000

    :white_check_mark: improving test coverage on user register resolvers

--- src/schema/users/Register/resolvers.test.ts
+++ src/schema/users/Register/resolvers.test.ts
@@ -1,6 +1,8 @@
+import faker from "faker";
 import { connect, disconnect } from "../../../database";
 import { graphqlRequest } from "../../../test-utils/graphqlRequest";
 import { generateUsers } from "../../../test-utils/userGen";
+import { MIN_PASSWORD_LENGTH } from "../../../utils/config";
 import { UserModel } from "../model";
 
 beforeAll(async () => {
@@ -87,18 +89,44 @@ describe("User register resolvers", () => {
         },
       },
     });
+  });
 
-    // Database check
-    const dbUser = await UserModel.find({ username: cleanedUsers[0].username });
-    expect(dbUser.length).toEqual(1);
-    expect(dbUser[0]).toEqual(expect.objectContaining(cleanedUsers[0]));
+  test("query getting one user fails", async () => {
+    // Database setup
+    await generateUsers(2);
+
+    // Request
+    const getUser = `
+      query getUser($username: String!) {
+        getUser(username: $username) {
+          errors {
+            message
+          }
+        }
+      }
+    `;
+    const response = await graphqlRequest({
+      source: getUser,
+      variableValues: {
+        username: "madeUpUser",
+      },
+    });
+
+    // Response check
+    expect(response).toMatchObject({
+      data: {
+        getUser: {
+          errors: [{ message: "User doesn't exist." }],
+        },
+      },
+    });
   });
 
   test("query who am I", async () => {
     // Database and user setup
     const { cleanedUsers, dbUsers } = await generateUsers();
 
-    // Request;
+    // Request
     const whoAmI = `
       query {
         whoAmI {
@@ -126,4 +154,257 @@ describe("User register resolvers", () => {
       },
     });
   });
+
+  test("mutation create user", async () => {
+    const user = {
+      username: faker.internet.userName(),
+      password: faker.internet.password(),
+    };
+
+    // Request
+    const createUser = `
+      mutation createUser(
+        $username: String!
+        $password: String!
+        ) {
+        createUser(username: $username, password: $password) {
+          user {
+            username
+          }
+        }
+      }
+    `;
+
+    const response = await graphqlRequest({
+      source: createUser,
+      variableValues: { username: user.username, password: user.password },
+    });
+
+    // Response check
+    expect(response).toMatchObject({
+      data: {
+        createUser: {
+          user: { username: user.username },
+        },
+      },
+    });
+
+    // Database check
+    const dbUser = await UserModel.findOne({ username: user.username });
+    expect(dbUser).toBeDefined();
+    expect(dbUser?.username).toEqual(user.username);
+  });
+
+  test("mutation create user fails with existing user", async () => {
+    const { users } = await generateUsers();
+
+    const user = {
+      username: users[0].username,
+      password: users[0].password,
+    };
+
+    // Request
+    const createUser = `
+      mutation createUser(
+        $username: String!
+        $password: String!
+        ) {
+        createUser(username: $username, password: $password) {
+          errors {
+            message
+          }
+        }
+      }
+    `;
+
+    const response = await graphqlRequest({
+      source: createUser,
+      variableValues: { username: user.username, password: user.password },
+    });
+
+    // Response check
+    expect(response).toMatchObject({
+      data: {
+        createUser: {
+          errors: [{ message: "Username already exists." }],
+        },
+      },
+    });
+  });
+
+  test("mutation create user fails with short password", async () => {
+    // Database setup
+    const user = {
+      username: faker.internet.userName(),
+      password: "blah",
+    };
+
+    // Request
+    const createUser = `
+      mutation createUser(
+        $username: String!
+        $password: String!
+        ) {
+        createUser(username: $username, password: $password) {
+          errors {
+            message
+          }
+        }
+      }
+    `;
+
+    const response = await graphqlRequest({
+      source: createUser,
+      variableValues: { username: user.username, password: user.password },
+    });
+
+    // Response check
+    expect(response).toMatchObject({
+      data: {
+        createUser: {
+          errors: [
+            {
+              message: `Password length is too short, minimum length is ${MIN_PASSWORD_LENGTH} chars.`,
+            },
+          ],
+        },
+      },
+    });
+  });
+
+  test("mutation delete user", async () => {
+    // Database setup
+    const { cleanedUsers, dbUsers } = await generateUsers();
+
+    // Request
+    const deleteUser = `
+      mutation deleteUser($username: String!) {
+        deleteUser(username: $username) {
+          user {
+            username
+            firstName
+            lastName
+            emailAddress
+          }
+        }
+      }
+    `;
+
+    const response = await graphqlRequest({
+      source: deleteUser,
+      variableValues: { username: cleanedUsers[0].username },
+      currentUser: dbUsers[0],
+    });
+
+    // Response check
+    expect(response).toMatchObject({
+      data: {
+        deleteUser: {
+          user: cleanedUsers[0],
+        },
+      },
+    });
+
+    // Database check
+    const deletedUser = await UserModel.findOne({
+      username: cleanedUsers[0].username,
+    });
+
+    expect(deletedUser).toBeNull();
+  });
+
+  test("mutation delete user fails doesn't exist", async () => {
+    // Database setup
+    const { dbUsers } = await generateUsers();
+
+    // Request
+    const deleteUser = `
+      mutation deleteUser($username: String!) {
+        deleteUser(username: $username) {
+          errors {
+            message
+          }
+        }
+      }
+    `;
+
+    const response = await graphqlRequest({
+      source: deleteUser,
+      variableValues: { username: "madeUpUser" },
+      currentUser: dbUsers[0],
+    });
+
+    // Response check
+    expect(response).toMatchObject({
+      data: {
+        deleteUser: {
+          errors: [{ message: "User doesn't exist." }],
+        },
+      },
+    });
+  });
+
+  test("mutation delete user fails not authorised", async () => {
+    // Database setup
+    const { dbUsers } = await generateUsers(2);
+
+    // Request
+    const deleteUser = `
+      mutation deleteUser($username: String!) {
+        deleteUser(username: $username) {
+          errors {
+            message
+          }
+        }
+      }
+    `;
+
+    const response = await graphqlRequest({
+      source: deleteUser,
+      variableValues: { username: dbUsers[1].username },
+      currentUser: dbUsers[0],
+    });
+
+    // Response check
+    expect(response).toMatchObject({
+      data: {
+        deleteUser: {
+          errors: [{ message: "Not authorised to carry out this action." }],
+        },
+      },
+    });
+  });
+
+  // TODO: finish this test
+  // test("mutation login user", async () => {
+  //   // Database setup
+  //   const { users } = await generateUsers(1);
+
+  //   // Request
+  //   const loginUser = `
+  //     mutation loginUser(
+  //         $username: String!
+  //         $password: String!
+  //       ) {
+  //       loginUser(username: $username, password: $password) {
+  //         token
+  //       }
+  //     }
+  //   `;
+
+  //   const response = await graphqlRequest({
+  //     source: deleteUser,
+  //     variableValues: {
+  //       username: users[0].username,
+  //       password: users[0].password,
+  //     },
+  //   });
+
+  //   // Response check
+  //   expect(response).toMatchObject({
+  //     data: {
+  //       token,
+  //     },
+  //   });
+  // });
 });
--- src/schema/users/Register/resolvers.ts
+++ src/schema/users/Register/resolvers.ts
@@ -5,7 +5,7 @@ import { sign } from "jsonwebtoken";
 
 import { JWT_SECRET, MIN_PASSWORD_LENGTH } from "../../../utils/config";
 import { UserModel } from "../model";
-import { UpdateUserInput, UsernamePasswordInput, UserResponse } from "../types";
+import { UpdateUserInput, UserResponse } from "../types";
 import { MyContext } from "../../../types";
 import { isError } from "../../../utils/typeGuards";
 import {
@@ -47,16 +47,18 @@ export class UserRegisterResolver {
   }
 
   @Mutation(() => UserResponse)
-  async createUser(@Arg("options") options: UsernamePasswordInput) {
-    const existingUser = await getUser(options.username);
+  async createUser(
+    @Arg("username") username: string,
+    @Arg("password") password: string
+  ) {
+    const existingUser = await getUser(username);
     if (existingUser) return userExistsError();
 
-    if (options.password.length < MIN_PASSWORD_LENGTH)
-      return passwordTooShortError();
+    if (password.length < MIN_PASSWORD_LENGTH) return passwordTooShortError();
 
-    const hashedPassword = await hash(options.password);
+    const hashedPassword = await hash(password);
     const user = new UserModel({
-      username: options.username,
+      username: username,
       password: hashedPassword,
     });
 
@@ -112,11 +114,14 @@ export class UserRegisterResolver {
   }
 
   @Mutation(() => UserResponse)
-  async loginUser(@Arg("options") options: UsernamePasswordInput) {
-    const user = await getUser(options.username);
+  async loginUser(
+    @Arg("username") username: string,
+    @Arg("password") password: string
+  ) {
+    const user = await getUser(username);
     if (!user) return missingUserError();
 
-    const valid = await passwordVerify(user.password, options.password);
+    const valid = await passwordVerify(user.password, password);
     if (!valid) return incorrectPasswordError();
 
     const userForToken = {
