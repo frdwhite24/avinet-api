509ab1acbaac6c769b196d4a5cf059fe3ffcad08
Fred White <fred-white@hotmail.co.uk>
2020-12-29 07:45:19 +0000

    :white_check_mark: add tests loginUser and updateUsername

--- src/schema/users/Register/resolvers.test.ts
+++ src/schema/users/Register/resolvers.test.ts
@@ -1,8 +1,10 @@
 import faker from "faker";
+import { verify } from "jsonwebtoken";
 import { connect, disconnect } from "../../../database";
 import { graphqlRequest } from "../../../test-utils/graphqlRequest";
 import { generateUsers } from "../../../test-utils/userGen";
-import { MIN_PASSWORD_LENGTH } from "../../../utils/config";
+import { MyToken } from "../../../types";
+import { JWT_SECRET, MIN_PASSWORD_LENGTH } from "../../../utils/config";
 import { UserModel } from "../model";
 
 beforeAll(async () => {
@@ -375,36 +377,239 @@ describe("User register resolvers", () => {
     });
   });
 
-  // TODO: finish this test
-  // test("mutation login user", async () => {
-  //   // Database setup
-  //   const { users } = await generateUsers(1);
+  test("mutation login user", async () => {
+    // Database setup
+    const { users, dbUsers } = await generateUsers();
 
-  //   // Request
-  //   const loginUser = `
-  //     mutation loginUser(
-  //         $username: String!
-  //         $password: String!
-  //       ) {
-  //       loginUser(username: $username, password: $password) {
-  //         token
-  //       }
-  //     }
-  //   `;
+    // Request
+    const loginUser = `
+      mutation loginUser(
+          $username: String!
+          $password: String!
+        ) {
+        loginUser(username: $username, password: $password) {
+          token
+        }
+      }
+    `;
 
-  //   const response = await graphqlRequest({
-  //     source: deleteUser,
-  //     variableValues: {
-  //       username: users[0].username,
-  //       password: users[0].password,
-  //     },
-  //   });
+    const response = await graphqlRequest({
+      source: loginUser,
+      variableValues: {
+        username: users[0].username,
+        password: users[0].password,
+      },
+    });
 
-  //   // Response check
-  //   expect(response).toMatchObject({
-  //     data: {
-  //       token,
-  //     },
-  //   });
-  // });
+    // Response check
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-assignment
+    const token = response?.data?.loginUser?.token;
+    const decodedToken = verify(token, JWT_SECRET) as MyToken;
+
+    expect(decodedToken.username).toBe(users[0].username);
+    expect(decodedToken.id).toBe(dbUsers[0]._id.toString());
+  });
+
+  test("mutation login user fails if doesn't exist", async () => {
+    // Request
+    const loginUser = `
+      mutation loginUser(
+          $username: String!
+          $password: String!
+        ) {
+        loginUser(username: $username, password: $password) {
+          errors {
+            message
+          }
+        }
+      }
+    `;
+
+    const response = await graphqlRequest({
+      source: loginUser,
+      variableValues: {
+        username: "madeUpUser",
+        password: "badpassword!",
+      },
+    });
+
+    // Response check
+    expect(response).toMatchObject({
+      data: {
+        loginUser: {
+          errors: [{ message: "User doesn't exist." }],
+        },
+      },
+    });
+  });
+
+  test("mutation login user fails if wrong password", async () => {
+    // Database setup
+    const { users } = await generateUsers();
+
+    // Request
+    const loginUser = `
+      mutation loginUser(
+          $username: String!
+          $password: String!
+        ) {
+        loginUser(username: $username, password: $password) {
+          errors {
+            message
+          }
+        }
+      }
+    `;
+
+    const response = await graphqlRequest({
+      source: loginUser,
+      variableValues: {
+        username: users[0].username,
+        password: "badpassword!",
+      },
+    });
+
+    // Response check
+    expect(response).toMatchObject({
+      data: {
+        loginUser: {
+          errors: [{ message: "Incorrect password." }],
+        },
+      },
+    });
+  });
+
+  test("mutation update username", async () => {
+    // Database setup
+    const { cleanedUsers, dbUsers } = await generateUsers();
+    const newUsername = "newUsername";
+
+    // Request
+    const updateUsername = `
+      mutation updateUsername(
+        $username: String!
+        $newUsername: String!
+      ) {
+        updateUsername(
+          username: $username,
+          newUsername: $newUsername
+        ) {
+          user {
+            username
+            firstName
+            lastName
+            emailAddress
+          }
+          token
+        }
+      }
+    `;
+
+    const response = await graphqlRequest({
+      source: updateUsername,
+      variableValues: {
+        username: cleanedUsers[0].username,
+        newUsername: newUsername,
+      },
+      currentUser: dbUsers[0],
+    });
+
+    // Response check
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-assignment
+    const responseUser = response?.data?.updateUsername?.user;
+    expect(responseUser).toMatchObject({
+      ...cleanedUsers[0],
+      username: newUsername,
+    });
+
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-assignment
+    const token = response?.data?.updateUsername?.token;
+    const decodedToken = verify(token, JWT_SECRET) as MyToken;
+
+    expect(decodedToken.username).toBe(newUsername);
+    expect(decodedToken.id).toBe(dbUsers[0]._id.toString());
+
+    // Database check
+    const updatedUser = await UserModel.findById(dbUsers[0]._id);
+    expect(updatedUser).toBeDefined();
+    expect(updatedUser?.username).toBe(newUsername);
+  });
+
+  test("mutation update username fails user doesn't exist", async () => {
+    // Database setup
+    const { dbUsers } = await generateUsers();
+
+    // Request
+    const updateUsername = `
+      mutation updateUsername(
+        $username: String!
+        $newUsername: String!
+      ) {
+        updateUsername(
+          username: $username,
+          newUsername: $newUsername
+        ) {
+          errors {
+            message
+          }
+        }
+      }
+    `;
+
+    const response = await graphqlRequest({
+      source: updateUsername,
+      variableValues: {
+        username: "madeUpUser",
+        newUsername: "anotherMadeUpUser",
+      },
+      currentUser: dbUsers[0],
+    });
+
+    expect(response).toMatchObject({
+      data: {
+        updateUsername: {
+          errors: [{ message: "User doesn't exist." }],
+        },
+      },
+    });
+  });
+
+  test("mutation update username fails not authorised", async () => {
+    // Database setup
+    const { dbUsers } = await generateUsers(2);
+
+    // Request
+    const updateUsername = `
+      mutation updateUsername(
+        $username: String!
+        $newUsername: String!
+      ) {
+        updateUsername(
+          username: $username,
+          newUsername: $newUsername
+        ) {
+          errors {
+            message
+          }
+        }
+      }
+    `;
+
+    const response = await graphqlRequest({
+      source: updateUsername,
+      variableValues: {
+        username: dbUsers[1].username,
+        newUsername: "newUsername",
+      },
+      currentUser: dbUsers[0],
+    });
+
+    expect(response).toMatchObject({
+      data: {
+        updateUsername: {
+          errors: [{ message: "Not authorised to carry out this action." }],
+        },
+      },
+    });
+  });
 });
